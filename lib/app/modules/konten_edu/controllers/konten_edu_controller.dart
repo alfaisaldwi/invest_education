3f013f3864b6e97a6787ebf8789eafdfb03c5af8
alfaisaldwi <alfaisaldwi@gmail.com>
2025-08-25 23:19:44 +0700

    update gradle & dependencies

--- lib/app/modules/konten_edu/controllers/konten_edu_controller.dart
+++ lib/app/modules/konten_edu/controllers/konten_edu_controller.dart
@@ -1,5 +1,5 @@
 import 'dart:io';
-import 'package:flutter_share/flutter_share.dart';
+import 'package:share_plus/share_plus.dart';
 import 'package:http/http.dart' as http;
 // import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -91,10 +91,10 @@ class KontenEduController extends GetxController {
   }
 
   void shareImage(String imagePath, String title) async {
-    await FlutterShare.shareFile(
+    await Share.shareFiles(
+      [imagePath],
       text: title,
-      title: title,
-      filePath: imagePath,
+      subject: title,
     );
   }
 
